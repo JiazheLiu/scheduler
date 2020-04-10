74f240a599339dc416b39b7fd1c2a77d995e195c
JiazheLiu <jiazheliu2021@u.northwestern.edu>
2020-04-10 15:25:01 -0500

    Slice 6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import 'rbx/index.css';
-import { Button, Container, Title } from 'rbx';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
+import "rbx/index.css";
+import { Button, Container, Message, Title } from "rbx";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-};
-const Banner = ({ title }) => (
-  <h1>{ title }</h1>
-);
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiQTPdkd6Wb2SE9AQKb1N2MryR27wcjEE",
+  authDomain: "schedule-cfe55.firebaseapp.com",
+  databaseURL: "https://schedule-cfe55.firebaseio.com",
+  projectId: "schedule-cfe55",
+  storageBucket: "schedule-cfe55.appspot.com",
+  messagingSenderId: "54025686110",
+  appId: "1:54025686110:web:77777d71ac188766e578cf",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
+const Banner = ({ title }) => (
+  <Title>{ title || '[loading...]' }</Title>
+);
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
@@ -20,65 +33,134 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
-  
-const Course = ({ course }) => (
-  <Button>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </Button>
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => course !== selection && courseConflict(course, selection))
 );
 
 const buttonColor = selected => (
   selected ? 'success' : null
+)
+
+const TermSelector = ({ state }) => (
+  <Button.Group hasAddons>
+    { Object.values(terms)
+        .map(value => 
+          <Button key={value}
+            color={ buttonColor(value === state.term) }
+            onClick={ () => state.setTerm(value) }
+            >
+            { value }
+          </Button>
+        )
+    }
+  </Button.Group>
 );
 
-const CourseList = ({ courses }) => {
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const saveCourse = (course, meets) => {
+  db.child('courses').child(course.id).update({meets})
+    .catch(error => alert(error));
+};
+
+const moveCourse = course => {
+  const meets = prompt('Enter new meeting data, in this format:', course.meets);
+  if (!meets) return;
+  const {days} = timeParts(meets);
+  if (days) saveCourse(course, meets); 
+  else moveCourse(course);
+};
+  
+const Course = ({ course, state, user }) => (
+    <Button color={ buttonColor(state.selected.includes(course)) }
+      onClick={ () => state.toggle(course) }
+      onDoubleClick={ user ? () => moveCourse(course) : null }
+      disabled={ hasConflict(course, state.selected) }
+      >
+      { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+    </Button>
+);
+
+const useSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
+  };
+  return [ selected, toggle ];
+};
+
+const CourseList = ({ courses, user }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
   const termCourses = courses.filter(course => term === getCourseTerm(course));
+  
   return (
     <React.Fragment>
       <TermSelector state={ { term, setTerm } } />
       <Button.Group>
-        { termCourses.map(course => <Course key={ course.id } course={ course } state={ { term, setTerm } } />) }
+        { termCourses.map(course =>
+           <Course key={ course.id } course={ course }
+             state={ { selected, toggle } } />) }
       </Button.Group>
     </React.Fragment>
   );
 };
 
-const TermSelector = ({ state }) => (
-  <Button.Group hasAddons>
-  { Object.values(terms)
-      .map(value => 
-        <Button key={value}
-          color={ buttonColor(value === state.term) }
-          onClick={ () => state.setTerm(value) }
-          >
-          { value }
-        </Button>
-      )
-  }
-  </Button.Group>
-);
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: Object.values(schedule.courses).map(addCourseTimes)
+});
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
+    const handleData = snap => {
+      if (snap.val()) setSchedule(addScheduleTimes(snap.val()));
     }
-    fetchSchedule();
-  }, [])
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
 
   return (
     <Container>
       <Banner title={ schedule.title } />
       <CourseList courses={ schedule.courses } />
     </Container>
-  );
+);
 };
 
 export default App;
